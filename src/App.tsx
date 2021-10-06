882ec1684540e3acd6d8f0be18b1b200f7eeb625
BrunoSobralDEV <brunosobralss@hotmail.com>
2021-10-06 12:09:02 -0300

    Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+//acessibilidade
+Modal.setAppElement('#root');
+
 export function App() {
-  return (
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    //só uma sugestão, sempre que pedir interação do usuário, colocar no nome da função a palavra "handle"
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+    
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+    
+    return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+      
+
       <GlobalStyle />
     </>
   );
